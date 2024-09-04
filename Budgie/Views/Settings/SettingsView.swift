edb1d5a5a2931f7573a1cd901e0c5564fc7fcd2d
Joseph Baldwin <joebaldwin@outlook.com>
2024-09-04 20:39:44 +0100

    Change of version number. Clarification of text regarding Apple Health setting.

--- Budgie/Views/Settings/SettingsView.swift
+++ Budgie/Views/Settings/SettingsView.swift
@@ -115,8 +115,8 @@ struct SettingsView: View {
                 }
             }
             
-            Section(header: Text("Apple Health"), footer: Text("Turning this on will stop Budgie Diet using data from Apple Health to set your budgets. It will also stop Budgie Diet adding new food eaten to Apple Health, but won't delete any old entries.")) {
-                Toggle("Ignore all Apple Health data", isOn: $manualMode)
+            Section(header: Text("Apple Health"), footer: Text("If Budgie Diet was granted access to your Apple Health data, turning this on will stop it from using or adding new data, but old food entries it's logged will stay. Your budgets will be based on the manual calorie burn figures set above, rather than real data.\n\nIf Budgie Diet doesn't have access to your Apple Health data, this setting won't change anything.")) {
+                Toggle("Ignore Apple Health data", isOn: $manualMode)
             }
             
             Section(header: Text("Other settings")) {
