d5dd3693cdf7e3310f94b29884472708a03ab3c5
Joseph Baldwin <joebaldwin@outlook.com>
2026-07-01 17:48:02 +0100

    Deduplication and rationalisation

--- Budgie/Views/Main page/Components/TodayFoodList.swift
+++ Budgie/Views/Main page/Components/TodayFoodList.swift
@@ -16,59 +16,60 @@
 import SwiftUI
 
 struct TodayFoodList: View {
-    @Environment(\.defaultMinListRowHeight) var minRowHeight
-    
     @EnvironmentObject var dataLump: TodayLump
-    @State var firstRow = true
+    
+    var groupedByMeal: [UUID: [CalorieEntry]] {
+        Dictionary(grouping: dataLump.foodList, by: \.meal)
+    }
+    
+    struct FoodHeaderView: View {
+        let name: String
+        let calsText: String
+        
+        var body: some View {
+            HStack {
+                Text(name.uppercased())
+                    .font(.subheadline)
+                    .foregroundStyle(.secondary)
+                Spacer()
+                Text(calsText.uppercased())
+                    .font(.subheadline)
+                    .foregroundStyle(.secondary)
+            }
+        }
+    }
     
     var body: some View {
-        Spacer()
         if dataLump.foodList.count != 0 || dataLump.healthKitCalories != 0 {
             if dataLump.foodList.count != 0 {
                 ForEach(dataLump.mealList) { meal in
-                    HStack {
-                        Text(meal.name.uppercased())
-                            .font(.subheadline)
-                            .foregroundStyle(.secondary)
-                        Spacer()
-                        if dataLump.mealList.first == meal {
-                            Text("CALORIES")
-                                .font(.subheadline)
-                                .foregroundStyle(.secondary)
-                        }
-                    }
-                    ForEach(dataLump.foodList) { entry in
-                        if entry.meal == meal.mealUUID {
+                    let entries = groupedByMeal[meal.mealUUID] ?? []
+                    if !entries.isEmpty {
+                        FoodHeaderView(name: meal.name, calsText: "CALORIES")
+                        ForEach(entries) { entry in
                             CalorieEntryView(calories: entry.calories, narrative: entry.narrative ?? "Quick calories", realEntry: entry.realEntry, date: entry.date)
                         }
-                    }
-                    HStack {
-                        Spacer()
-                        VStack {
-                            Divider()
-                                .frame(maxWidth: 50)
+                        HStack {
+                            Spacer()
+                            VStack {
+                                Divider()
+                                    .frame(maxWidth: 50)
+                            }
                         }
-                    }
-                    HStack {
+                        HStack {
+                            Spacer()
+                            Text("**\((dataLump.mealTotalList[meal.mealUUID] ?? 0).formatted())**")
+                        }
                         Spacer()
-                        Text("**\(dataLump.mealTotalList[meal.mealUUID]!.formatted())**")
                     }
-                    Spacer()
                 }
             }
             
             if dataLump.healthKitCalories != 0 {
                 Spacer()
-                HStack {
-                    Text("FROM OTHER APPS")
-                        .font(.subheadline)
-                        .foregroundStyle(.secondary)
-                    Spacer()
-                        Text("CALORIES")
-                            .font(.subheadline)
-                            .foregroundStyle(.secondary)
-                }
-                CalorieEntryView(calories: dataLump.healthKitCalories, narrative: "Calories from Apple Health", realEntry: false, date: getStartOfDay(date: Date()))
+                FoodHeaderView(name: "FROM OTHER APPS", calsText: "CALORIES")
+                // "Date" doesn't matter too much because it doesn't display where "realEntry" is false so we can pass whatever we want.
+                CalorieEntryView(calories: dataLump.healthKitCalories, narrative: "Calories from Apple Health", realEntry: false, date: dataLump.lastUpdate)
                 Spacer()
             }
         } else {
